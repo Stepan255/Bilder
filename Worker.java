ab01dd9a8749de02f4b9b88f7d8727b9016e14da
Stepan <St3Ba4@gmail.com>
2022-06-27 09:51:56 +0300

    add adding arguments

--- Worker.java
+++ Worker.java
@@ -24,5 +24,83 @@ public class Worker {
 		super();
 	}
 
+	public Worker arg0(int arg0){
+		this.arg0 = arg0;
+		return this;
+	}
 
+	public Worker arg1(String arg1){
+		this.arg1 = arg1;
+		return this;
+	}
+
+	public Worker arg2(Boolean arg2){
+		this.arg2 = arg2;
+		return this;
+	}
+
+	public Worker arg3(Foo arg3){
+		this.arg3 = arg3;
+		return this;
+	}
+
+	public Worker arg4(String arg4){
+		this.arg4 = arg4;
+		return this;
+	}
+
+	public Worker arg5(Calendar arg5){
+		this.arg5 = arg5;
+		return this;
+	}
+
+	public Worker arg6(String arg6){
+		this.arg6 = arg6;
+		return this;
+	}
+
+	public Worker arg7(int arg7){
+		this.arg7 = arg7;
+		return this;
+	}
+
+	public Worker arg8(ArrayList<Integer> arg8){
+		this.arg8 = arg8;
+		return this;
+	}
+
+	public Worker arg9(Character arg9){
+		this.arg9 = arg9;
+		return this;
+	}
+
+	public Worker arg10(int arg10){
+		this.arg10 = arg10;
+		return this;
+	}
+
+	public Worker arg11(int arg11){
+		this.arg11 = arg11;
+		return this;
+	}
+
+	public Worker arg12(String arg12){
+		this.arg12 = arg12;
+		return this;
+	}
+
+	public Worker arg13(int arg13){
+		this.arg13 = arg13;
+		return this;
+	}
+
+	public Worker arg14(int arg14){
+		this.arg14 = arg14;
+		return this;
+	}
+
+	public Worker arg15(int arg15){
+		this.arg15 = arg15;
+		return this;
+	}
 }
