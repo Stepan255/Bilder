b47fdef09f34c616640be3e66dcbd8a29973bc30
Stepan <St3Ba4@gmail.com>
2022-06-27 09:50:16 +0300

    add class Worker

---
+++ Worker.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class Worker {
+	private int arg0;
+	private String arg1;
+	private Boolean arg2;
+	private Foo arg3;
+	private String arg4;
+	private Calendar arg5;
+	private String arg6;
+	private int arg7;
+	private ArrayList<Integer> arg8;
+	private Character arg9;
+	private int arg10;
+	private int arg11;
+	private String arg12;
+	private int arg13;
+	private int arg14;
+	private int arg15;
+
+	public Worker() {
+		super();
+	}
+
+
+}
